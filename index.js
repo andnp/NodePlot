c30c4c5f4ab0490b0192e34fb353f008ac07d7e5
andy <andy.patterson@genesys.com>
2017-07-31 16:44:56 -0400

    set up for npm repo

---
+++ index.js
@@ -0,0 +1,7 @@
+import Operations from './src/Operation';
+import './src/DataLoader';
+import './src/Aggregates';
+import './src/LinePlots';
+import './src/plotters/LocalPlotter';
+
+export default Operations;
